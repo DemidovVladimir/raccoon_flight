5a79a01d0ee04dbdde4ccdc7fef8e912cfbf58ef
Vladimir Demidov <uncojet@gmail.com>
2023-03-30 23:50:23 +0200

    cleanup

--- model.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-class Model:
-    @staticmethod
-    def read():
-        try:
-            return pd.read_csv('data.csv')
-        except FileNotFoundError:
-            print('File not found')
